24c4c0b05f3635c948e187446d50523101aad7d4
Bram Adams <3282661+bramses@users.noreply.github.com>
2023-02-13 02:23:02 -0500

    - footnotes - yaml for images moth and year in ghost

--- src/methods/publishPost.ts
+++ src/methods/publishPost.ts
@@ -1,25 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { SettingsProp, ContentProp, DataProp } from "./../types/index";
-import {
-	MarkdownView,
-	Notice,
-	request,
-	TFile,
-	getLinkpath,
-	parseLinktext,
-	requestUrl,
-	RequestUrlParam
-} from "obsidian";
+import { MarkdownView, Notice, request } from "obsidian";
 import { sign } from "jsonwebtoken";
 
 const md_footnote = require("markdown-it-footnote");
 const matter = require("gray-matter");
-const MarkdownIt = require("markdown-it")
-
+const MarkdownIt = require("markdown-it");
 
 const md = new MarkdownIt({
 	html: true,
 }).use(md_footnote);
+
 const version = "v4";
 
 const contentPost = (frontmatter: ContentProp, data: DataProp) => ({
@@ -31,6 +22,41 @@ const contentPost = (frontmatter: ContentProp, data: DataProp) => ({
 	],
 });
 
+const replaceFootnotesWithHTMLCard = (content: string) => {
+	// Ghost swallows the footnote links for some reason, so we need to replace them with a HTML card
+	/*
+	<hr class="footnotes-sep">
+	<section class="footnotes">
+	<ol class="footnotes-list">
+	<li id="fn1" class="footnote-item"><p>test <a href="#fnref1" class="footnote-backref">↩︎</a></p>
+	</li>
+	<li id="fn2" class="footnote-item"><p>test2 <a href="#fnref2" class="footnote-backref">↩︎</a></p>
+	</li>
+	</ol>
+	</section>
+
+	needs to be surrounded with `<!--kg-card-begin: html-->` and `<!--kg-card-end: html-->`
+	*/
+
+
+
+	const footnotes = content.match(
+		/<hr class="footnotes-sep">(.*)<\/section>/s
+	);
+	if (footnotes) {
+		const htmlCard = `<!--kg-card-begin: html--><div class="kg-card-markdown">${footnotes[0]}</div><!--kg-card-end: html-->`;
+		content = content.replace(
+			/<hr class="footnotes-sep">(.*)<\/section>/s,
+			htmlCard
+		);
+
+		// remove the footnote links
+		content = content.replace(/<a href="#fnref.*<\/a>/g, "");
+	}
+
+	return content;
+};
+
 const openInBrowser = (url: string) => {
 	const a = document.createElement("a");
 	a.href = url;
@@ -38,7 +64,6 @@ const openInBrowser = (url: string) => {
 	a.rel = "noopener noreferrer";
 	a.click();
 };
-
 
 export const publishPost = async (
 	view: MarkdownView,
@@ -71,6 +96,8 @@ export const publishPost = async (
 		feature_image: metaMatter?.feature_image || undefined,
 		updated_at: metaMatter?.updated_at || undefined,
 		"date modified": metaMatter["date modified"] || undefined,
+		imagesYear: metaMatter["ghost-images-year"] || undefined,
+		imagesMonth: metaMatter["ghost-images-month"] || undefined,
 	};
 
 	const BASE_URL = settings.baseURL;
@@ -79,19 +106,47 @@ export const publishPost = async (
 	const content = data.content.replace(
 		/!*\[\[(.*?)\]\]/g,
 		(match: any, p1: string) => {
-			if (p1.toLowerCase().includes(".png") || p1.toLowerCase().includes(".jpg") || p1.toLowerCase().includes(".jpeg") || p1.toLowerCase().includes(".gif")) {
+			if (
+				p1.toLowerCase().includes(".png") ||
+				p1.toLowerCase().includes(".jpg") ||
+				p1.toLowerCase().includes(".jpeg") ||
+				p1.toLowerCase().includes(".gif")
+			) {
 				try {
-					console.log("match ", p1);
-					// get the year
-					const year = new Date().getFullYear();
-					// get the month
-					const monthNum = new Date().getMonth() + 1;
-					let month = monthNum.toString();
-					if (monthNum < 10) {
-						month = `0${monthNum}`;
+					let year;
+					let month;
+					if (frontmatter.imagesYear && frontmatter.imagesMonth) {
+						year = frontmatter.imagesYear;
+						month = frontmatter.imagesMonth;
+
+						if (month < 10) {
+							month = `0${month}`;
+						}
+
+						console.log("year", year);
+						console.log("month", month);
+					} else {
+						// get the year
+						year = new Date().getFullYear();
+						// get the month
+						const monthNum = new Date().getMonth() + 1;
+						month = monthNum.toString();
+						if (monthNum < 10) {
+							month = `0${monthNum}`;
+						}
 					}
 
-					return `<figure class="kg-card kg-image-card"><img src="${BASE_URL}/content/images/${year}/${month}/${p1.replace(/ /g, "-").replace(/%20/g, "-")}" alt="${BASE_URL}/content/images/${year}/${month}/${p1.replace(/ /g, "-").replace(/%20/g, "-")}"></img><figcaption>${p1}</figcaption></figure>`;
+					return `<figure class="kg-card kg-image-card"><img src="${BASE_URL}/content/images/${year}/${month}/${p1
+						.replace(/ /g, "-")
+						.replace(
+							/%20/g,
+							"-"
+						)}" alt="${BASE_URL}/content/images/${year}/${month}/${p1
+						.replace(/ /g, "-")
+						.replace(
+							/%20/g,
+							"-"
+						)}"></img><figcaption>${p1}</figcaption></figure>`;
 				} catch (err) {
 					console.log("is404Req", err);
 				}
@@ -119,10 +174,12 @@ export const publishPost = async (
 		}
 	);
 
-
 	// take the url from view tweet format and replace the entire blockquote with a tweet embed iframe
 	// add a new line before every ([View Tweet]
-	data.content = data.content.replace(/\(\[View Tweet\]/gm, "\n([View Tweet]");
+	data.content = data.content.replace(
+		/\(\[View Tweet\]/gm,
+		"\n([View Tweet]"
+	);
 
 	data.content = data.content.replace(
 		/(^>.*\n.*)*\(https:\/\/twitter.com\/(.*)\/status\/(\d+)\)\)/gm,
@@ -130,94 +187,112 @@ export const publishPost = async (
 			console.log("p1", p1);
 			console.log("p2", p2);
 
-
 			const url = `https://twitter.com/${p2}/status/${p3}?ref_src=twsrc%5Etfw`;
 			return `<figure class="kg-card kg-embed-card"><div class="twitter-tweet twitter-tweet-rendered"><iframe src="${url}" width="550" height="550" frameborder="0" scrolling="no" allowfullscreen="true" style="border: none; max-width: 100%; min-width: 100%;"></iframe></div></figure>`;
 		}
-	)
+	);
 
-	// use the ghosts admin /post api to see if post with slug exists
-	const slugExistsRes = await request({
-		url: `${settings.url}/ghost/api/${version}/admin/posts/?source=html&filter=slug:${frontmatter.slug}`,
-		method: "GET",
-		contentType: "application/json",
-		headers: {
-			"Access-Control-Allow-Methods": "GET",
-			"Content-Type": "application/json;charset=utf-8",
-			Authorization: `Ghost ${token}`,
-		},
-	});
+	const sendToGhost = true; // set to false to test locally
 
-	const slugExists = JSON.parse(slugExistsRes).posts.length > 0;
-
-	if (slugExists) {
-		// get id of post if it exists
-		const id = JSON.parse(slugExistsRes).posts[0].id;
-		console.log("slug exists -- updating post:" + id);
-
-		// add updated_at iso string to frontmatter
-		frontmatter.updated_at = JSON.parse(slugExistsRes).posts[0].updated_at;
-		
-		console.log(contentPost(frontmatter, data));
-
-
-
-		// if slug exists, update the post
-		const result = await request({
-			url: `${settings.url}/ghost/api/${version}/admin/posts/${id}/?source=html`,
-			method: "PUT",
+	if (sendToGhost) {
+		// use the ghosts admin /post api to see if post with slug exists
+		const slugExistsRes = await request({
+			url: `${settings.url}/ghost/api/${version}/admin/posts/?source=html&filter=slug:${frontmatter.slug}`,
+			method: "GET",
 			contentType: "application/json",
 			headers: {
-				"Access-Control-Allow-Methods": "PUT",
+				"Access-Control-Allow-Methods": "GET",
 				"Content-Type": "application/json;charset=utf-8",
 				Authorization: `Ghost ${token}`,
 			},
-			body: JSON.stringify(contentPost(frontmatter, data)),
 		});
 
-		console.log(result)
+		const slugExists = JSON.parse(slugExistsRes).posts.length > 0;
 
-		const json = JSON.parse(result);
+		if (slugExists) {
+			// get id of post if it exists
+			const id = JSON.parse(slugExistsRes).posts[0].id;
+			console.log("slug exists -- updating post:" + id);
 
-		if (json?.posts) {
-			new Notice(
-				`"${json?.posts?.[0]?.title}" update has been ${json?.posts?.[0]?.status} successful!`
+			// add updated_at iso string to frontmatter
+			frontmatter.updated_at =
+				JSON.parse(slugExistsRes).posts[0].updated_at;
+
+			const htmlContent = contentPost(frontmatter, data);
+			htmlContent.posts[0].html = replaceFootnotesWithHTMLCard(
+				htmlContent.posts[0].html
 			);
-			// https://bram-adams.ghost.io/ghost/#/editor/post/63d3246b7932ae003df67c64
-			openInBrowser(`${settings.url}/ghost/#/editor/post/${json?.posts?.[0]?.id}`);
+			console.log("htmlContent", htmlContent);
+			// if slug exists, update the post
+			const result = await request({
+				url: `${settings.url}/ghost/api/${version}/admin/posts/${id}/?source=html`,
+				method: "PUT",
+				contentType: "application/json",
+				headers: {
+					"Access-Control-Allow-Methods": "PUT",
+					"Content-Type": "application/json;charset=utf-8",
+					Authorization: `Ghost ${token}`,
+				},
+				body: JSON.stringify(htmlContent),
+			});
+
+			// console.log(contentPost(frontmatter, data));
+
+			const json = JSON.parse(result);
+
+			if (json?.posts) {
+				new Notice(
+					`"${json?.posts?.[0]?.title}" update has been ${json?.posts?.[0]?.status} successful!`
+				);
+				// https://bram-adams.ghost.io/ghost/#/editor/post/63d3246b7932ae003df67c64
+				openInBrowser(
+					`${settings.url}/ghost/#/editor/post/${json?.posts?.[0]?.id}`
+				);
+			} else {
+				console.log(
+					`${json.errors[0]?.details[0].message} - ${json.errors[0]?.details[0].params.allowedValues}`
+				);
+				console.log(
+					`${json.errors[0].context || json.errors[0].message}`
+				);
+			}
 		} else {
-			console.log(`${json.errors[0]?.details[0].message} - ${json.errors[0]?.details[0].params.allowedValues}`)
-			console.log(`${json.errors[0].context || json.errors[0].message}`)
-		}
-
-	} else {
-		// upload post
-		const result = await request({
-			url: `${settings.url}/ghost/api/${version}/admin/posts/?source=html`,
-			method: "POST",
-			contentType: "application/json",
-			headers: {
-				"Access-Control-Allow-Methods": "POST",
-				"Content-Type": "application/json;charset=utf-8",
-				Authorization: `Ghost ${token}`,
-			},
-			body: JSON.stringify(contentPost(frontmatter, data)),
-		});
-
-		const json = JSON.parse(result);
-
-		if (json?.posts) {
-			new Notice(
-				`"${json?.posts?.[0]?.title}" has been ${json?.posts?.[0]?.status} successful!`
+			const htmlContent = contentPost(frontmatter, data);
+			htmlContent.posts[0].html = replaceFootnotesWithHTMLCard(
+				htmlContent.posts[0].html
 			);
-			openInBrowser(`${settings.url}/ghost/#/editor/post/${json?.posts?.[0]?.id}`);
-		} else {
-			new Notice(`${json.errors[0].context || json.errors[0].message}`);
-			new Notice(
-				`${json.errors[0]?.details[0].message} - ${json.errors[0]?.details[0].params.allowedValues}`
-			);
-		}
+			// upload post
+			const result = await request({
+				url: `${settings.url}/ghost/api/${version}/admin/posts/?source=html`,
+				method: "POST",
+				contentType: "application/json",
+				headers: {
+					"Access-Control-Allow-Methods": "POST",
+					"Content-Type": "application/json;charset=utf-8",
+					Authorization: `Ghost ${token}`,
+				},
+				body: JSON.stringify(htmlContent),
+			});
 
-		return json;
+			const json = JSON.parse(result);
+
+			if (json?.posts) {
+				new Notice(
+					`"${json?.posts?.[0]?.title}" has been ${json?.posts?.[0]?.status} successful!`
+				);
+				openInBrowser(
+					`${settings.url}/ghost/#/editor/post/${json?.posts?.[0]?.id}`
+				);
+			} else {
+				new Notice(
+					`${json.errors[0].context || json.errors[0].message}`
+				);
+				new Notice(
+					`${json.errors[0]?.details[0].message} - ${json.errors[0]?.details[0].params.allowedValues}`
+				);
+			}
+
+			return json;
+		}
 	}
 };
